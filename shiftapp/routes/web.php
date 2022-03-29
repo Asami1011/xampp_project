13a99fff6452809ebea8e43de9bd6e941a338366
Asami1011 <kuutan.ranran.1011@gmail.com>
2022-03-29 18:53:12 +0900

    ルーティングテスト

--- shiftapp/routes/web.php
+++ shiftapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//以下を追加
+//↓これはLaravel ver.7まで
+//Route::get('/test/home', 'TestController@entrance');
+//↓Laravel ver.8以降～
+Route::get('/test/home', 'App\Http\Controllers\TestController@entrance');
+/*
+Route::get('/test/home', function () {
+    return view('sample');
+});
+*/
+//useでコントローラーをインポートする場合
+//Route::get('/test/home', [TestController::class, 'entrance']);
